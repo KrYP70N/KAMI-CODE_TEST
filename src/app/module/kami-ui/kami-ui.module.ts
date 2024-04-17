db39be08b0504960cf5204f005a881f415c0b4c2
Aiden <krypn22@gmail.com>
2024-04-18 04:13:54 +0700

    add style config

--- src/app/module/kami-ui/kami-ui.module.ts
+++ src/app/module/kami-ui/kami-ui.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { TableComponent } from './table/table.component';
+
+const components = [
+  CardComponent,
+  TableComponent
+]
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CardComponent],
-  exports: [CardComponent]
+  declarations: components,
+  exports: components
 })
 
 export class KAMI_UI { }
