9d286ee2afdb7884836cd3a595739459511e7923
Aiden <krypn22@gmail.com>
2024-04-18 01:06:15 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KAMI_UI } from './module/kami-ui/kami-ui.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KAMI_UI],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/module/kami-ui/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'card',
+  templateUrl: 'card.component.html',
+  styleUrl: './card.component.scss'
+})
+
+export class CardComponent {
+
+}
